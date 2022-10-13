65d66ea01fbf89a5f2a2f57b60b2767d09d7f65f
prafulgabhane <prafulgabhane786@gmail.com>
2022-10-13 16:18:49 +0530

    Adding code to main branch

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserResgistration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration\n");
+
+            RegexPattern.FirstName();
+            Console.WriteLine("\nUser Registration Program Ends");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegistration/RegexPattern.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserResgistration
+{
+    public class RegexPattern
+    {
+        //UC-1 Program to enter valid first name format.
+        
+        public static void FirstName()
+        {
+            string[] arr = { "Praful", "praful", "pr12", "PRaful123", "12345" }; 
+            string pattern = "^[A-Z][a-z]{3}";
+
+            foreach (string input in arr)
+            {
+                //IsMatch(String, String, RegexOptions)
+
+                if (Regex.IsMatch(input, pattern))
+                {
+                    Console.WriteLine("{0} is valid", input);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is Invalid", input);
+                }
+            }
+        }
+    }
+}
